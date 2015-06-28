71cc1b10c55201a879b92b78f84afecd1ac9b2b1
LLstarla <loraleestarla@me.com>
2015-06-28 10:27:52 -0700

    lll

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,1 @@
 $(function() { $.scrollIt(); });
-
-$(document).ready(function(){
-    $('#gallery').least();
-    'random': true/false (default to true);
-'lazyload': true/false  (default to true);
-'scrollToGallery': true/false (default to true);
-});
