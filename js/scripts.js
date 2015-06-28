6e4e0768fb329d2b2427c2ab0c9a7aff01036c4d
LLstarla <loraleestarla@me.com>
2015-06-28 10:27:19 -0700

    jhja

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,7 @@ $(function() { $.scrollIt(); });
 
 $(document).ready(function(){
     $('#gallery').least();
+    'random': true/false (default to true);
+'lazyload': true/false  (default to true);
+'scrollToGallery': true/false (default to true);
 });
